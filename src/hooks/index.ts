0f2f7c533d44cfd0d271e49674b1462baf8a3d91
mohammad seyfollahi <ui28@fardup.co>
2026-06-06 14:42:13 +0330

    chore: remove duplicate hook file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { default as useQueryParams } from "./useQueryParams";
 export { default as useSortParams } from "./useSortParams";
+export { default as useURLParams } from "./useURLParams";
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export { default as URLParamsProvider } from "./provider/provider";
-export { default as useQueryParams } from "./hooks/useQueryParams";
-export { default as useSortParams } from "./hooks/useSortParams";
-export { default as useURLParams } from "./hooks/useURLParams";
+export { useQueryParams, useSortParams, useURLParams } from "./hooks";
 export type { SortDir, SortGroup, TURLParamsContext, UseQueryParams, UseSortParams, UseURLParams } from "./types/types";
